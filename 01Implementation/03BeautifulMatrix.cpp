7483d72395caf2d64ae14f890bbce1c4d6b716f2
Shivaprasad-S-Devadiga <Shivaprasada02@gmail.com>
2025-10-30 15:51:22 +0530

    Beauti mat

---
+++ 01Implementation/03BeautifulMatrix.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int matrix[5][5];
+    int row, col;
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> matrix[i][j];
+            if (matrix[i][j] == 1) {
+                row = i + 1;
+                col = j + 1;
+            }
+        }
+    }
+
+    cout << abs(row - 3) + abs(col - 3);
+    return 0;
+}
