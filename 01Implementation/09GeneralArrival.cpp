108705ecb8090c9f09bf43d1801f720f1b7ee0c2
Shivaprasad-S-Devadiga <Shivaprasada02@gmail.com>
2025-11-01 00:21:33 +0530

    genaral arrival

---
+++ 01Implementation/09GeneralArrival.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio();
+  cin.tie(nullptr);
+
+  int n;
+  cin>>n;
+
+  int smallNum, smallI=1, maxNum, maxI=1;
+  cin>>smallNum;
+  maxNum = smallNum;
+  int temp;
+  for(int i=2; i<=n; i++){
+    cin>>temp;
+    if(temp<=smallNum){
+      smallNum= temp;
+      smallI = i;
+    }
+
+    if(temp>maxNum){
+      maxNum= temp;
+      maxI = i;
+    }
+
+  }
+  int swap = (maxI-1) + (n-smallI);
+  
+  cout<<(maxI-1) + (n-smallI) - (maxI>smallI ? 1:0);
+
+  return 0;
+
+}
